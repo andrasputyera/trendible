3a9e29883aac53a6e6a7cb02d0d4b7b86d31e15a
andrasputyera <andras.putyera@gmail.com>
2021-06-19 23:46:51 -0400

    started global navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <router-link class="header" :to="{ name: 'Home' }">Trendible</router-link>
+            </div>
+            <div class="nav-links">
+                <ul>
+                    <router-link class="link" :to="#">Home</router-link>
+                    <router-link class="link" :to="#">Articles</router-link>
+                    <router-link class="link" :to="#">Create Post</router-link>
+                    <router-link class="link" :to="#">Login/Register</router-link>
+                </ul>
+            </div>
+        </nav>
+        <menuIcon/>
+    </header>
+</template>
+
+<script>
+import menuIcon from '../assets/Icons/bars-regular.svg';
+
+export default {
+    name: 'navigation',
+    components: {
+        menuIcon
+    }  
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
