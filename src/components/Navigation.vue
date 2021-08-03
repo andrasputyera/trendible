95821d6d186b35fdc511fa6e9fed422bd51fe4f7
andrasputyera <andras.putyera@gmail.com>
2021-08-03 23:36:17 +0200

    completed navigation for desktop & mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
                 <router-link class="header" :to="{ name: 'Home' }">Trendible</router-link>
             </div>
             <div class="nav-links">
-                <ul>
+                <ul v-show="!mobile">
                     <router-link class="link" to="#">Home</router-link>
                     <router-link class="link" to="#">Articles</router-link>
                     <router-link class="link" to="#">Create Post</router-link>
@@ -13,9 +13,9 @@
                 </ul>
             </div>
         </nav>
-        <menuIcon class="menu-icon" />
+        <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile" />
         <transition name="mobile-nav">
-            <ul class="mobile-nav">
+            <ul class="mobile-nav" v-show="mobileNav">
                 <router-link class="link" to="#">Home</router-link>
                 <router-link class="link" to="#">Articles</router-link>
                 <router-link class="link" to="#">Create Post</router-link>
@@ -32,6 +32,33 @@ export default {
     name: 'navigation',
     components: {
         menuIcon
+    },
+    data() {
+        return {
+            mobile: null,
+            mobileNav: null,
+            windowWidth: null,
+        }
+    },
+    created() {
+        window.addEventListener('resize', this.checkScreen);
+        this.checkScreen();
+    },
+    methods: {
+        checkScreen() {
+            this.windowWidth = window.innerWidth;
+            if (this.windowWidth <= 750) {
+                this.mobile = true;
+                return;
+            }
+            this.mobile = false;
+            this.mobileNav = false;
+            return;
+        },
+
+        toggleMobileNav() {
+            this.mobileNav = !this.mobileNav
+        }
     }  
 };
 </script>
@@ -115,6 +142,23 @@ header {
             padding: 15px 0;
             color: #fff;
         }
+    }
+
+    .mobile-nav-enter-active,
+    .mobile-nav-leave-active {
+        transition: all 1s ease;
+    }
+
+    .mobile-nav-enter {
+        transform: translateX(-250px);
+    }
+
+    .mobile-nav-enter-to {
+        transform: translateX(0);
+    }
+
+    .mobile-nav-leave-to {
+        transform: translateX(-250px);
     }
 }
 </style>
