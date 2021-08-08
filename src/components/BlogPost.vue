e53ae32c5d61bd3c58a92cb70bda0f050e7d720c
andrasputyera <andras.putyera@gmail.com>
2021-08-07 23:02:59 -0400

    continued the structure for the blog posts

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blog-wrapper">
+      <div class="blog-content">
+          <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+          <h2 v-else>{{ post.title }}</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "blogPost",
+    props: ["post"]
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
