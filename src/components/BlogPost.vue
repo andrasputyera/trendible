ac8cffe77b6aab6b953502d1c47ff32e8de7a1e9
andrasputyera <andras.putyera@gmail.com>
2021-08-13 23:06:07 -0400

    continued to update the BlogPost component

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -16,7 +16,7 @@
       </div>
       <div class="blog-photo">
         <img v-if="post.welcomeScreen" :src="require(`../assets/blogPhotos/${post.photo}.jpg`)" alt="" />
-        <img v-else :src="post.blogCoverPhoto" alt="" />
+        <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt="" />
       </div>
   </div>
 </template>
@@ -34,4 +34,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.blog-wrapper {
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  @media (min-width: 700px) {
+    min-height: 650px;
+    max-height: 650px;
+    flex-direction: row;
+  }
+
+  .blog-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      flex: 4;
+      order: 2;
+      @media (min-width: 700px) {
+          order: 1;
+      }
+      @media (min-width: 800px) {
+          flex: 3;
+      }
+  }
+}
+
+
 </style>
