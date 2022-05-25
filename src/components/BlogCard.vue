065b23567faf339d1e3a2ea0a48b9c20b8733cc8
andrasputyera <andras.putyera@gmail.com>
2022-05-25 23:23:51 +0200

    started creating the blog card component

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+    h3 {
+        font-weight: 300;
+        font-size: 28px;
+        margin-bottom: 32px;
+    }
+}
+</style>
